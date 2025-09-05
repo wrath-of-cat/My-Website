0f9f1cc05103728a13d4c88f7f471fbdb342ea1a
Yasharth Srivastava <srivastavayashu15@gmail.com>
2025-09-05 17:28:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+/*console.log(`Hello, My name is Yasharth.`);
+console.log(`This is my website.`);
+
+window.alert(`Welcome to the website!`);
+
+document.getElementById(`name`).textContent = "Yasharth Srivastava";*/
